f9edd65af4179b0b418e51ffcae0ff8ce2897d5b
Antoine Wacheux <awacheux.utc@gmail.com>
2025-11-30 21:19:59 +0100

    Handle multiple clients

--- app/main.go
+++ app/main.go
@@ -21,24 +21,30 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	client, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
 
+	for {
+		client, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleClient(client)
+	}
+}
+
+func handleClient(client net.Conn) {
 	for {
 		// Read from the client
 		_, err := client.Read(make([]byte, 1024))
 		if err != nil {
 			fmt.Println("Error reading from client: ", err.Error())
-			os.Exit(1)
+			return
 		}
 
 		_, err = client.Write([]byte("+PONG\r\n"))
 		if err != nil {
 			fmt.Println("Failed to write to client: ", err.Error())
-			os.Exit(1)
+			return
 		}
 	}
 }
