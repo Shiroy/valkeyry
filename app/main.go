1a12c7c9b753dce72dde617a7754c76b21ba03c9
Antoine Wacheux <awacheux.utc@gmail.com>
2025-11-30 21:07:36 +0100

    Respond to pong

--- app/main.go
+++ app/main.go
@@ -21,9 +21,15 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	client, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	_, err = client.Write([]byte("+PONG\r\n"))
+	if err != nil {
+		fmt.Println("Failed to write to client: ", err.Error())
 		os.Exit(1)
 	}
 }
